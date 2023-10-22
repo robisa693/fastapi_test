b0a83d2ba0cac5d143192e8855b2a8eab56dfd7f
robisa693 <roberto_isaac@live.se>
2023-10-23 00:02:26 +0200

    learning more fun stuff, how to handle parameters with pydantic!

--- main.py
+++ main.py
@@ -27,8 +27,36 @@ items = {
 @app.get("/")
 def index() -> dict[str, dict[int, Item]]:
     return {"items": items}
+
 @app.get("/items/{item_id}")
 def query_item_by_id(item_id: int) -> Item:
     if item_id not in items:
         raise HTTPException(status_code=404, detail=f"Item ID {item_id} not found")
     return items[item_id]
+
+Selection = dict[
+    str, str | int | float | Category | None
+]
+
+@app.get("/items/")
+def query_items_by_paramters( #validate input  paramters
+    name: str | None = None, 
+    price: float | None = None, 
+    count: int | None = None, 
+    category: Category | None = None,
+                            ) -> dict[str, Selection | list[Item]]:
+    def check_item(item: Item) -> bool: #function that checks paramters matches from the outer scope
+        return all(
+            (
+                name is None or item.name == name,
+                price is None or item.price == price,
+                count is None or item.count == count,
+                category is None or item.category == category,
+            )
+        )
+    selection = [item for item in items.values() if check_item(item)]
+    return {
+        "query": {"name": name, "price": price, "count": count, "category": category},
+        "selection": selection
+         }
+    
--- test_fastapi_server.py
+++ test_fastapi_server.py
@@ -3,3 +3,5 @@ import requests
 print(requests.get("http://127.0.0.1:8000").json())
 print("\n")
 print(requests.get("http://127.0.0.1:8000/items/0").json())
+print("\n")
+print(requests.get("http://127.0.0.1:8000/items?name=Nails").json())
