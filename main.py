cc705082a05d5d244928111ed576698c12156692
robisa693 <roberto_isaac@live.se>
2023-10-22 23:30:22 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from enum import Enum
+
+app = FastAPI()
+
+class Category(Enum):
+    TOOLS = 'tools'
+    CONSUMABLES = 'consumables'
+
+class Item(BaseModel):
+    name: str
+    price: float
+    count: int
+    id: int
+    category: Category
+
+items = {
+    0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS),
+    1: Item(name="Pliers", price=5.99, count=20, id=1, category=Category.TOOLS),
+    2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
+}
+
+
+# FastAPI handles JSON serialization and deserialization for us.
+# We can simply use built-in python and Pydantic types, in this case dict[int, Item].
+@app.get("/")
+def index() -> dict[str, dict[int, Item]]:
+    return {"items": items}
+@app.get("/items/{item_id}")
+def query_item_by_id(item_id: int) -> Item:
+    if item_id not in items:
+        raise HTTPException(status_code=404, detail=f"Item ID {item_id} not found")
+    return items[item_id]
---
+++ test_fastapi_server.py
@@ -0,0 +1,5 @@
+import requests
+
+print(requests.get("http://127.0.0.1:8000").json())
+print("\n")
+print(requests.get("http://127.0.0.1:8000/items/0").json())
